d8a206ea00eda006ddf51651a656a27b71ded970
Rodolfo Espinoza <rodolfoeslo@gmail.com>
2020-03-04 23:33:32 -0600

    TASK 4 ->Now the styling becomes important but not that much, so just go ahead and pick a free template from https://creative-tim.com and apply it. You should style it in a way that the final product resembles a Wizard component.

---
+++ WEB/Views/Shared/Components/WebComicContent2/WebComicContent2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB.Views.Shared.Components.WebComic
+{
+    [ViewComponent(Name = "WebComicContent2")]
+    public class WebComicContent2ViewComponent: ViewComponent
+    {
+        public WebComicContent2ViewComponent()
+        { }
+
+        public async Task<IViewComponentResult> InvokeAsync(Domain.Model.WebComic webComic )        
+        {
+            return View(webComic);
+        }
+    }
+}
